77bafa809921fcc778c8d2ef5609c22607baac1d
Elvis <elvis@ezsolucoes.com.br>
2023-09-23 02:43:36 -0300

    refactor(AppModule): altera o modulo para receber TaskModule.

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -2,19 +2,16 @@ import 'package:flutter_modular/flutter_modular.dart';
 
 import 'const/app_routes.dart';
 
-import 'modules/task_module/controller/task_controller.dart';
-import '../modules/task_module/view/task_page.dart';
+import 'modules/task_module/task_module/task_module.dart';
 
 class AppModule extends Module {
   @override
   final List<Bind> binds = [
-    Bind((i) => TaskController()),
   ];
 
   @override
   List<ModularRoute> get routes => [
-    ChildRoute(AppRoutes.appDefaultPage, child: (context, args) => TaskPage()),
-    ChildRoute(AppRoutes.appTaskPage, child: (context, args) => TaskPage()),
+    ModuleRoute(AppRoutes.appDefaultPage, module: TaskModule()),
   ];
 
 }
